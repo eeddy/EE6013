bc0856e539a5815fafd69812c40729e5cb14e770
Amir Reza Hariri <amirrezahariri2000@gmail.com>
2025-02-13 11:00:50 -0400

    Add files via upload
    ADC ESP32

---
+++ adc.ino
@@ -0,0 +1,18 @@
+const int adcPin = 36; 
+int adcValue = 0;      
+
+void setup() {
+    Serial.begin(115200);  
+}
+
+void loop() {
+    adcValue = analogRead(adcPin); 
+    float voltage = adcValue * (3.3 / 4095.0); 
+
+    Serial.print("ADC Value: ");
+    Serial.print(adcValue);
+    Serial.print(" | Voltage: ");
+    Serial.println(voltage, 3); 
+
+    delay(100); 
+}
